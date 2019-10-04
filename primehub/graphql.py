7edfbc0759cd96391bc3d8749d67086ca5665501
Kent Huang <kentwelcome@gmail.com>
2019-10-04 16:06:46 +0800

    [Fix] remove unused data
    Signed-off-by: Kent Huang <kentwelcome@gmail.com>

--- primehub/graphql.py
+++ primehub/graphql.py
@@ -1,8 +1,6 @@
 import requests
 
-url = 'https://hub.kent.dev.primehub.io/graphql'
 payload = {'query': '{groups{name enabledSharedVolume} datasets{name type volumeName} users{username}}'}
-secret = '1228101f93b387e08a1bb99a53b363f2036678d565d3321cf522a2af8ca84c93'
 headers = {'Authorization': 'Bearer %s'%secret}
 
 def get_primehub_info(url, secret):
